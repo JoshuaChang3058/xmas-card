a41887008ccd1116af95cfa9136c45cb34b55e01
dafeng306 <75168543+dafeng306@users.noreply.github.com>
2020-12-16 09:11:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+function 第一段2 () {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Breve))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Breve))
+    music.playTone(698, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Breve))
+}
+function 第一段3 () {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Breve))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Breve))
+    music.playTone(698, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Breve))
+}
+function 第一小節 () {
+	
+}
+function 第一段 () {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Breve))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Breve))
+    music.playTone(698, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Breve))
+}
+basic.showNumber(input.lightLevel())
+basic.pause(1000)
+basic.forever(function () {
+    if (input.lightLevel() > 30) {
+        第一段2()
+    }
+})
